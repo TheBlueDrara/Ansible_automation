1a5daa816558b8d17c3b01aca24a15a2517fd003
soony0737@gmail.com <soony0737@gmail.com>
2025-06-05 13:47:10 +0300

    modifed the start script

---
+++ automation/start.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+##################### Start Safe Header ######################
+#Developed by Alex Umansky aka TheBlueDrara
+#Porpuse: Initialize the start of the lab and run the main playbook to deploy app
+#Date: 4.6.2025
+#Version 0.0.1
+set -o nounset
+set -o errexit
+set -o pipefail
+#################### End Safe Header ##########################
+source .
+
+
+function main(){
+
+# Fails if the script runs on non Debian based distros.
+
+    if [[ $EUID -ne 0 ]]; then
+        echo "Running on Debian-family distro. Executing main code..."
+    else
+        exit 1
+    fi
+
+    deploy_lab
+    
+}
+
+
+function deploy_lab(){
+
+    docker compose up -d
+    docker exec -it docker-ansible-host-1 git clone https://github.com/TheBlueDrara/Ansible_automation.git
+
+
+}
+
+main
